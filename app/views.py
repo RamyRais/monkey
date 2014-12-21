117b853b855ba48941334fe667735d0dc9bb6baa
Ramy Rais <ramy.rais@gmail.com>
2014-12-21 21:22:56 +0100

    added the edit profile fonctionnality and improve the interface using bootsrap

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for
 from app import app, db
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, EditForm
 from app.models import Monkey
 from config import MONKEYS_PER_PAGE
 
@@ -29,3 +29,35 @@ def profile(name=""):
         return redirect(url_for('index'))
     return render_template('profile.html', monkey=monkey,
                            title='{0} profile'.format(monkey.name))
+
+@app.route('/profile/<name>/edit', methods=['GET', 'POST'])
+def edit_profile(name=""):
+    monkey = Monkey.query.filter_by(name=name).first()
+    if monkey is None:
+        return redirect(url_for('index'))
+    form = EditForm()
+    if form.validate_on_submit():
+        number_errors = 0
+        if monkey.email != form.email.data: #testing email unicity
+            if Monkey.query.filter_by(email=form.email.data).first():
+                form.email.errors.append("Email {0} is already used".format(
+                    form.email.data))
+                number_errors += 1
+        if monkey.name != form.name.data: #testing name unicity
+            if Monkey.query.filter_by(name=form.name.data).first():
+                form.name.errors.append("Name {0} is already used".format(
+                    form.name.data))
+                number_errors += 1
+        if number_errors > 0:
+            return render_template('edit.html', form=form)       
+        monkey.email = form.email.data
+        monkey.name = form.name.data
+        monkey.age = form.age.data
+        db.session.add(monkey)
+        db.session.commit()
+        return redirect(url_for('profile', name=monkey.name))
+    else:
+        form.name.data = monkey.name
+        form.email.data = monkey.email
+        form.age.data = monkey.age
+    return render_template('edit.html', form=form)
